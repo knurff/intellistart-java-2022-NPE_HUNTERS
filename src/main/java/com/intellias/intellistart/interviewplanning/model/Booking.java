2e7cdd1e7bde5e5fe2c56b6a8f66bfd7df50df3a
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-10-06 16:11:48 +0300

    Fix Booking model

--- src/main/java/com/intellias/intellistart/interviewplanning/model/Booking.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/Booking.java
@@ -1,12 +1,19 @@
 package com.intellias.intellistart.interviewplanning.model;
 
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
 
 /**
  * Booking entity.
  */
+@Entity
+@Data
 public class Booking {
+  @Id @GeneratedValue
   private Long id;
-  private Set<Slot> candidateId;
-  private Set<Slot> interviewerId;
+  //private Set<Slot> candidateId;
+  //private Set<Slot> interviewerId;
 }
