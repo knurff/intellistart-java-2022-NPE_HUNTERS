ab517c71bfd256f6ded05985236bc9d42e5b9ba9
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-10-06 16:06:43 +0300

    Fix annotation on Booking entity

--- src/main/java/com/intellias/intellistart/interviewplanning/model/Booking.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/Booking.java
@@ -1,14 +1,11 @@
 package com.intellias.intellistart.interviewplanning.model;
 
 import java.util.Set;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 /**
  * Booking entity.
  */
 public class Booking {
-  @GeneratedValue
   private Long id;
   private Set<Slot> candidateId;
   private Set<Slot> interviewerId;
