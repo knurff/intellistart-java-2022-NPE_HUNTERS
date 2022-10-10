fa307d6128cd5e49f06e4502bd5e3968bd752dc9
Andrii Kozubenko <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-10-10 19:34:31 +0300

    Try fix build fail issue caused by unavailable database connection

--- src/test/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplicationTests.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplicationTests.java
@@ -3,7 +3,6 @@ package com.intellias.intellistart.interviewplanning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class InterviewPlanningApplicationTests {
 
   @Test
