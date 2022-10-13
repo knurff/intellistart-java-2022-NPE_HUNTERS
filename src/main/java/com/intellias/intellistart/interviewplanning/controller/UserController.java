0e025321ccbea88cc7c173adfc2875ec8db48129
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-13 23:13:23 +0300

    Add several helper methods for UserController in DateUtils

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
 import com.intellias.intellistart.interviewplanning.util.DateUtils;
-import java.time.LocalDate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +12,11 @@ public class UserController {
 
   @GetMapping("weeks/current")
   public int getCurrentWeek() {
-    return DateUtils.getWeekFrom(DateUtils.NUMBERING_FROM, LocalDate.now());
+    return DateUtils.getCurrentWeek();
+  }
+
+  @GetMapping("weeks/next")
+  public int getNextWeek() {
+    return DateUtils.getNextWeek();
   }
 }
--- src/main/java/com/intellias/intellistart/interviewplanning/util/DateUtils.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/util/DateUtils.java
@@ -9,7 +9,6 @@ import java.time.temporal.WeekFields;
  * Util class, which contains methods for getting information about
  * current and next week numbers.
  */
-
 public final class DateUtils {
 
   /**
@@ -18,9 +17,9 @@ public final class DateUtils {
   private DateUtils() {}
 
   /**
-   * Default date from which numbering begins(better to move it to config).
+   * Default date from which numbering begins.
    */
-  public static final LocalDate NUMBERING_FROM = LocalDate.of(2022, 1, 1);
+  private static final LocalDate NUMBERING_FROM = LocalDate.of(2022, 1, 1);
 
   /**
    * Returns week number of {@code current}, relative to {@code from}.
@@ -50,5 +49,23 @@ public final class DateUtils {
    */
   public static int getNextWeekFrom(LocalDate from, LocalDate current) {
     return getWeekFrom(from, current) + 1;
+  }
+
+  /**
+   * Returns week number of today's date, relative to {@code NUMBERING_FROM}.
+   *
+   * @return the week number(1-INT_MAX)
+   */
+  public static int getCurrentWeek() {
+    return getWeekFrom(NUMBERING_FROM, LocalDate.now());
+  }
+
+  /**
+   * Returns next week number of today's date, relative to {@code NUMBERING_FROM}.
+   *
+   * @return the next week number(2-INT_MAX)
+   */
+  public static int getNextWeek() {
+    return getNextWeekFrom(NUMBERING_FROM, LocalDate.now());
   }
 }
