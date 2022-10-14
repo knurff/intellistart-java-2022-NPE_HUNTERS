87176f4c2ef1d55bba78c003f002c195c31164be
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-10-14 14:37:58 +0300

    Add formatted response in UserController

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
+import com.intellias.intellistart.interviewplanning.response.GetWeekResponse;
 import com.intellias.intellistart.interviewplanning.util.DateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   @GetMapping("weeks/current")
-  public int getCurrentWeek() {
-    return DateUtils.getCurrentWeek();
+  public GetWeekResponse getCurrentWeek() {
+    return new GetWeekResponse(DateUtils.getCurrentWeek());
   }
 
   @GetMapping("weeks/next")
-  public int getNextWeek() {
-    return DateUtils.getNextWeek();
+  public GetWeekResponse getNextWeek() {
+    return new GetWeekResponse(DateUtils.getNextWeek());
   }
 }
