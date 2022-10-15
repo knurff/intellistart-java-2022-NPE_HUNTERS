1d875cecfdd314c280e9f5d8a38fcff51ad11f75
andrii_manzar <manzar20011@gmail.com>
2022-10-15 14:38:02 +0300

    Add exceptions to validate CandidateSlot

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/exception/InvalidCandidateSlotDateException.java
@@ -0,0 +1,12 @@
+package com.intellias.intellistart.interviewplanning.exception;
+
+/**
+ * Exception for invalid CandidateSlot date.
+ */
+
+public class InvalidCandidateSlotDateException extends RuntimeException {
+
+  public InvalidCandidateSlotDateException(String message) {
+    super(message);
+  }
+}
