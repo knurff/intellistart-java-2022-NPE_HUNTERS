8d6ee58e15d621b9d9e5fb11c7a3bb4b1ec269b6
knurff <beryndaviktor@gmail.com>
2022-10-15 17:46:21 +0300

    Implement getAll() method in In InterviewerController

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/InterviewerController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/InterviewerController.java
@@ -1,14 +1,42 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
+import com.intellias.intellistart.interviewplanning.model.InterviewerSlot;
 import com.intellias.intellistart.interviewplanning.model.User;
+import com.intellias.intellistart.interviewplanning.controller.dto.InterviewerSlotDto;
+import com.intellias.intellistart.interviewplanning.controller.dto.creator.InterviewerSlotsDtoCreator;
+import com.intellias.intellistart.interviewplanning.service.InterviewerService;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * InterviewerController controller.
  */
 @RestController
+@RequestMapping("/interviewers")
+@AllArgsConstructor
 public class InterviewerController {
+
+  private final InterviewerService interviewerService;
+  private final InterviewerSlotsDtoCreator interviewerSlotsDtoCreator;
+
+  /**
+   * Returns all interviewer slots by id.
+   *
+   * @param interviewerId long id of interviewer
+   * @return list of InterviewerSlotDto
+   */
+  @GetMapping("/{interviewerId}/slots")
+  public List<InterviewerSlotDto> getAllInterviewerSlots(@PathVariable Long interviewerId) {
+    List<InterviewerSlot> interviewerSlots =
+        interviewerService.getAllInterviewerSlotsByInterviewerId(interviewerId);
+
+    return interviewerSlotsDtoCreator.createInterviewerSlotsDtoList(interviewerSlots);
+  }
 
 }
