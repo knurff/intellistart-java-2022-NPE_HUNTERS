46b2b3ce41af247bbd027910b4a4aafd16ebb499
andrii_manzar <manzar20011@gmail.com>
2022-10-15 15:35:29 +0300

    Add implementation for getAllSlots, addSlot and editSlot methods in CandidateController

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/CandidateController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/CandidateController.java
@@ -1,11 +1,61 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
+import static com.intellias.intellistart.interviewplanning.util.CandidateSlotMapper.mapDtoToEntity;
+
+import com.intellias.intellistart.interviewplanning.model.CandidateSlot;
+import com.intellias.intellistart.interviewplanning.model.dto.CandidateSlotDto;
+import com.intellias.intellistart.interviewplanning.service.CandidateService;
+import com.intellias.intellistart.interviewplanning.util.CandidateSlotMapper;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * CandidateController controller.
  */
 @RestController
+@RequestMapping("/candidates/current/slots")
 public class CandidateController {
 
+  private final CandidateService candidateService;
+
+  @Autowired
+  public CandidateController(CandidateService candidateService) {
+    this.candidateService = candidateService;
+  }
+
+  @GetMapping
+  public List<CandidateSlotDto> getAllSlots() {
+    List<CandidateSlot> candidateSlotList = candidateService.getAllSlots(getUserEmailFromToken());
+    return CandidateSlotMapper.convertCandidateSlotListToDtoList(candidateSlotList);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public CandidateSlot addSlot(@RequestBody CandidateSlotDto candidateSlotDto) {
+    candidateSlotDto.setEmail(getUserEmailFromToken());
+    return candidateService.createSlot(mapDtoToEntity(candidateSlotDto));
+  }
+
+  @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void editSlot(@PathVariable Long id, @RequestBody CandidateSlotDto candidateSlotDto) {
+    candidateSlotDto.setEmail(getUserEmailFromToken());
+    candidateService.editSlot(mapDtoToEntity(candidateSlotDto), id);
+  }
+
+  private String getUserEmailFromToken() {
+    Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+    return loggedInUser.getName();
+  }
 }
--- src/main/java/com/intellias/intellistart/interviewplanning/model/dto/CandidateSlotDto.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/dto/CandidateSlotDto.java
@@ -1,11 +1,35 @@
 package com.intellias.intellistart.interviewplanning.model.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.intellias.intellistart.interviewplanning.model.Booking;
+import com.intellias.intellistart.interviewplanning.model.TimePeriod;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * CandidateSlot dto.
  */
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"email", "date", "period"})
+@RequiredArgsConstructor
 public class CandidateSlotDto {
 
+  @JsonInclude(value = Include.NON_NULL)
+  private Long id;
+
+  private String email;
+
+  private LocalDate date;
+
+  private TimePeriod period;
+
+  @JsonInclude(value = Include.NON_EMPTY)
+  private Set<Booking> bookings;
 }
