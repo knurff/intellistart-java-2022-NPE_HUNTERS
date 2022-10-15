c8692b9aabad254f1ca13839cbe8a41b926a45c7
andrii_manzar <manzar20011@gmail.com>
2022-10-15 15:13:11 +0300

    Add implementation of getAllSlots, createSlot and editSlot methods in CandidateService

--- src/main/java/com/intellias/intellistart/interviewplanning/service/CandidateService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/service/CandidateService.java
@@ -1,12 +1,15 @@
 package com.intellias.intellistart.interviewplanning.service;
 
-import com.intellias.intellistart.interviewplanning.model.Booking;
+import com.intellias.intellistart.interviewplanning.exception.SlotContainsBookingsException;
+import com.intellias.intellistart.interviewplanning.exception.SlotNotFoundException;
 import com.intellias.intellistart.interviewplanning.model.CandidateSlot;
 import com.intellias.intellistart.interviewplanning.repository.CandidateSlotRepository;
-import java.util.ArrayList;
+import com.intellias.intellistart.interviewplanning.service.validator.CandidateSlotValidator;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * CandidateService service.
@@ -14,22 +17,61 @@ import org.springframework.stereotype.Service;
 @Service
 public class CandidateService {
 
+  private final CandidateSlotRepository candidateSlotRepository;
+
   @Autowired
-  private CandidateSlotRepository candidateSlotRepository;
-
-
-  public CandidateSlot createSlot() {
-    return new CandidateSlot();
+  public CandidateService(CandidateSlotRepository candidateSlotRepository) {
+    this.candidateSlotRepository = candidateSlotRepository;
   }
 
-  public boolean editSlot() {
+  /**
+   * Returns all CandidateSlot's by email.
+   */
+
+  public List<CandidateSlot> getAllSlots(String email) {
+    return candidateSlotRepository.findAllByEmail(email);
+  }
+
+  /**
+   * Validates and creates CandidateSlot.
+   */
+
+  public CandidateSlot createSlot(CandidateSlot candidateSlot) {
+    validateAndSaveCandidateSlot(candidateSlot);
+    return candidateSlot;
+  }
+
+  /**
+   * Validates and updates existing CandidateSlot.
+   */
+
+  @Transactional
+  public boolean editSlot(CandidateSlot candidateSlot, Long id) {
+    checkThatCandidateSlotExistsWithNoBookings(id);
+    candidateSlot.setId(id);
+    validateAndSaveCandidateSlot(candidateSlot);
     return true;
   }
 
-  public List<CandidateSlot> getAllSlots() {
-    return new ArrayList<>();
+  private void checkThatCandidateSlotExistsWithNoBookings(Long id) {
+    Optional<CandidateSlot> candidateSlot = candidateSlotRepository.findById(id);
+    if (candidateSlot.isEmpty()) {
+      throw new SlotNotFoundException(
+          String.format("Candidate slot with id: %d does not exist", id));
+    } else if (candidateSlotContainsBookings(candidateSlot)) {
+      throw new SlotContainsBookingsException("This slot contains bookings, you cannot change it");
+    }
   }
 
+  private void validateAndSaveCandidateSlot(CandidateSlot candidateSlot) {
+    List<CandidateSlot> anotherCandidateSlots = candidateSlotRepository.findAllByEmail(
+        candidateSlot.getEmail());
+    CandidateSlotValidator.validate(candidateSlot, anotherCandidateSlots, candidateSlot.getId());
+    candidateSlotRepository.save(candidateSlot);
+  }
+
+  private boolean candidateSlotContainsBookings(Optional<CandidateSlot> candidateSlot) {
+    return !candidateSlot.get().getBookings().isEmpty();
+  }
 
 }
-
