0571f5e382890a10452a55454b3e4f5d08b72576
andriimanzar <manzar20011@gmail.com>
2022-10-23 14:32:23 +0300

    Add implementation for deleteBooking method

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/CoordinatorController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/CoordinatorController.java
@@ -4,25 +4,29 @@ import com.intellias.intellistart.interviewplanning.controller.dto.DashboardDto;
 import com.intellias.intellistart.interviewplanning.controller.dto.InterviewerSlotDto;
 import com.intellias.intellistart.interviewplanning.model.InterviewerSlot;
 import com.intellias.intellistart.interviewplanning.model.User;
+import com.intellias.intellistart.interviewplanning.service.BookingService;
 import com.intellias.intellistart.interviewplanning.service.CoordinatorService;
 import com.intellias.intellistart.interviewplanning.util.MappingUtils;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * CoordinatorController controller.
  */
 @RestController
+@AllArgsConstructor
 public class CoordinatorController {
-  private final CoordinatorService coordinatorService;
 
-  public CoordinatorController(final CoordinatorService coordinatorService) {
-    this.coordinatorService = coordinatorService;
-  }
+  private final CoordinatorService coordinatorService;
+  private final BookingService bookingService;
 
   @GetMapping("/users/interviewers")
   public List<User> getAllInterviewers() {
@@ -43,8 +47,8 @@ public class CoordinatorController {
    * Returns InterviewerSlotDto relative to {@code slot}.
    *
    * @param interviewerId slot owner interviewerId
-   * @param slotId id of interviewer slot
-   * @param slot new interviewerSlot
+   * @param slotId        id of interviewer slot
+   * @param slot          new interviewerSlot
    * @return interviewerSlotDto as updated slot
    */
   @PostMapping("/interviewers/{interviewerId}/slots/{slotId}")
@@ -54,5 +58,14 @@ public class CoordinatorController {
 
     InterviewerSlot responseEntity = coordinatorService.editSlot(interviewerId, slotId, entity);
     return MappingUtils.mapToInterviewerSlotDto(responseEntity);
+  }
+
+  /**
+   * Handles DELETE requests and deletes booking by id.
+   */
+  @DeleteMapping("/bookings/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteBooking(@PathVariable Long id) {
+    bookingService.deleteBooking(id);
   }
 }
