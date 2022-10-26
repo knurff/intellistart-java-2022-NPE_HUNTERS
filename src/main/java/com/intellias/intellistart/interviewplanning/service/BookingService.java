88c92b3426ef99510f532cc0fd05ab7dd12a58f2
andriimanzar <manzar20011@gmail.com>
2022-10-26 21:02:56 +0300

    Add implementation for createBooking in CoordinatorController

--- src/main/java/com/intellias/intellistart/interviewplanning/service/BookingService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/service/BookingService.java
@@ -1,12 +1,20 @@
 package com.intellias.intellistart.interviewplanning.service;
 
+import com.intellias.intellistart.interviewplanning.exception.BookingLimitExceededException;
 import com.intellias.intellistart.interviewplanning.exception.BookingNotFoundException;
+import com.intellias.intellistart.interviewplanning.exception.SlotNotFoundException;
 import com.intellias.intellistart.interviewplanning.model.Booking;
 import com.intellias.intellistart.interviewplanning.model.CandidateSlot;
 import com.intellias.intellistart.interviewplanning.model.InterviewerSlot;
+import com.intellias.intellistart.interviewplanning.model.User;
 import com.intellias.intellistart.interviewplanning.repository.BookingRepository;
+import com.intellias.intellistart.interviewplanning.repository.CandidateSlotRepository;
+import com.intellias.intellistart.interviewplanning.repository.InterviewerSlotRepository;
+import com.intellias.intellistart.interviewplanning.repository.UserRepository;
+import com.intellias.intellistart.interviewplanning.service.validator.BookingValidator;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -21,6 +29,9 @@ import org.springframework.stereotype.Service;
 public class BookingService {
 
   private final BookingRepository bookingRepository;
+  private final UserRepository userRepository;
+  private final CandidateSlotRepository candidateSlotRepository;
+  private final InterviewerSlotRepository interviewerSlotRepository;
 
   /**
    * Returns a set of booking ids which a related to a provided interviewer slot.
@@ -62,6 +73,18 @@ public class BookingService {
   }
 
   /**
+   * Inserts slots into booking, validates it and creates if valid.
+   */
+  public Booking createBooking(Booking booking, Long interviewerSlotId, Long candidateSlotId) {
+    setInterviewerSlotForBooking(booking, interviewerSlotId);
+    setCandidateSlotForBooking(booking, candidateSlotId);
+    Long interviewerId = booking.getInterviewerSlot().getInterviewer().getId();
+    checkThatMaxBookingIsNotExceeded(booking, interviewerId);
+    BookingValidator.validate(booking);
+    return bookingRepository.save(booking);
+  }
+
+  /**
    * Deletes booking by id.
    *
    * @param bookingId long id of booking
@@ -71,6 +94,41 @@ public class BookingService {
     checkThatBookingExists(bookingId);
     bookingRepository.deleteById(bookingId);
     return true;
+  }
+
+  private void setCandidateSlotForBooking(Booking booking, Long candidateSlotId) {
+    Optional<CandidateSlot> candidateSlot = candidateSlotRepository.findById(
+        candidateSlotId);
+    if (candidateSlot.isEmpty()) {
+      throw new SlotNotFoundException(
+          String.format("Cannot find candidate slot with id: %d", candidateSlotId));
+    } else {
+      booking.setCandidateSlot(candidateSlot.get());
+    }
+  }
+
+  private void setInterviewerSlotForBooking(Booking booking, Long interviewerSlotId) {
+    Optional<InterviewerSlot> interviewerSlot = interviewerSlotRepository.findById(
+        interviewerSlotId);
+    if (interviewerSlot.isEmpty()) {
+      throw new SlotNotFoundException(
+          String.format("Cannot find interviewer slot with id: %d", interviewerSlotId));
+    } else {
+      booking.setInterviewerSlot(interviewerSlot.get());
+    }
+  }
+
+  private void checkThatMaxBookingIsNotExceeded(Booking booking, Long interviewerId) {
+    User user = userRepository.findById(interviewerId).get();
+
+    long numberOfAllBookingsForThisWeek = bookingRepository.countAllInterviewerBookingsOnThisWeek(
+        booking.getInterviewerSlot().getWeek(), interviewerId);
+
+    if (numberOfAllBookingsForThisWeek + 1
+        > user.getMaxBookingsPerWeek()) {
+      throw new BookingLimitExceededException(
+          String.format("Booking limit for interviewer with id: %d exceeded", interviewerId));
+    }
   }
 
   private void checkThatBookingExists(Long bookingId) {
