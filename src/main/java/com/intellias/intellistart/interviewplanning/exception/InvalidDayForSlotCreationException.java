72d6278853706cb63e936dae7831379f6a143880
andriimanzar <manzar20011@gmail.com>
2022-10-29 23:30:25 +0300

    Add InvalidDayForSlotCreationException and handler for it

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/exception/InvalidDayForSlotCreationException.java
@@ -0,0 +1,11 @@
+package com.intellias.intellistart.interviewplanning.exception;
+
+/**
+ * Exception for invalid day of slot creation.
+ */
+public class InvalidDayForSlotCreationException extends RuntimeException {
+
+  public InvalidDayForSlotCreationException(String message) {
+    super(message);
+  }
+}
