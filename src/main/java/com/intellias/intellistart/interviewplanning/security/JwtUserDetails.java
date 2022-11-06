e5010445a5d6fdf2dd82fedf8242b61bb163df4e
andriimanzar <manzar20011@gmail.com>
2022-11-06 14:48:03 +0200

    Add custom implementation for JwtUserDetails and JwtUserDetailsService

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/security/JwtUserDetails.java
@@ -0,0 +1,53 @@
+package com.intellias.intellistart.interviewplanning.security;
+
+import java.util.Collection;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Custom implementation of {@link UserDetails}.
+ */
+@AllArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+  private final String email;
+  private final String password;
+  private final Collection<? extends GrantedAuthority> authorities;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+}
