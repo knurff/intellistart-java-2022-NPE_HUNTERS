221dd5cf792ddd590c28cd792ee80026828d739b
andriimanzar <manzar20011@gmail.com>
2022-11-06 14:48:33 +0200

    Add classes that works with Facebook token and response

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/dto/FacebookTokenDto.java
@@ -0,0 +1,18 @@
+package com.intellias.intellistart.interviewplanning.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * FacebookToken dto.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FacebookTokenDto {
+
+  private String token;
+}
