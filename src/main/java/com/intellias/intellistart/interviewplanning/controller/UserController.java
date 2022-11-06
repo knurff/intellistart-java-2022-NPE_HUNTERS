74793343837b8b6b633222a5b5b79f744eb4edf3
andriimanzar <manzar20011@gmail.com>
2022-11-06 14:52:14 +0200

    Move response package inside controller package

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
-import com.intellias.intellistart.interviewplanning.response.GetWeekResponse;
+import com.intellias.intellistart.interviewplanning.controller.response.GetWeekResponse;
 import com.intellias.intellistart.interviewplanning.util.DateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
