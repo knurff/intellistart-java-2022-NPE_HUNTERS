2043cb8a2af5a9ff58ec29786917e9d25b81b719
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-19 13:24:13 +0200

    Fix issue with converter, add tests

--- src/main/java/com/intellias/intellistart/interviewplanning/util/DayOfWeekConvertor.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/util/DayOfWeekConvertor.java
@@ -5,11 +5,15 @@ import java.time.DayOfWeek;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAccessor;
+import java.util.Locale;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 
 /**
  * DayOfWeekConvertor convertor class.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DayOfWeekConvertor {
 
 
@@ -22,7 +26,10 @@ public class DayOfWeekConvertor {
    */
   public static DayOfWeek stringToDayOfWeek(String day) {
     try {
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE");
+      DateTimeFormatter formatter = DateTimeFormatter
+          .ofPattern("EEEE")
+          .withLocale(Locale.ENGLISH);
+
       TemporalAccessor accessor = formatter.parse(day);
       return DayOfWeek.from(accessor);
     } catch (DateTimeParseException e) {
