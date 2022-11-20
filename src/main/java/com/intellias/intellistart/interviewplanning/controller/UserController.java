817b94240eed34cb6cd47cf611b209e9624d4f04
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-20 22:27:11 +0200

    Add getMe method in UserController (#94)
    * Refactor 'get week' methods
    
    * Rename UserDto to EmailDto
    
    * Add core functionality
    
    * Fix issue in javadoc

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.intellias.intellistart.interviewplanning.controller;
 
-import com.intellias.intellistart.interviewplanning.controller.response.GetWeekResponse;
+import com.intellias.intellistart.interviewplanning.controller.dto.UserDto;
+import com.intellias.intellistart.interviewplanning.controller.dto.WeekDto;
+import com.intellias.intellistart.interviewplanning.security.JwtUserDetails;
 import com.intellias.intellistart.interviewplanning.util.DateUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-  @GetMapping("weeks/current")
-  public GetWeekResponse getCurrentWeek() {
-    return new GetWeekResponse(DateUtils.getCurrentWeek());
+  @GetMapping("/weeks/current")
+  public WeekDto getCurrentWeek() {
+    return new WeekDto(DateUtils.getCurrentWeek());
   }
 
-  @GetMapping("weeks/next")
-  public GetWeekResponse getNextWeek() {
-    return new GetWeekResponse(DateUtils.getNextWeek());
+  @GetMapping("/weeks/next")
+  public WeekDto getNextWeek() {
+    return new WeekDto(DateUtils.getNextWeek());
+  }
+
+  /**
+   * Returns data about logged-in user.
+   *
+   * @return UserDto with data about logged-in user
+   */
+  @GetMapping("/me")
+  public UserDto getMe() {
+    JwtUserDetails details = (JwtUserDetails) SecurityContextHolder.getContext()
+        .getAuthentication().getPrincipal();
+
+    return new UserDto(details);
   }
 }
