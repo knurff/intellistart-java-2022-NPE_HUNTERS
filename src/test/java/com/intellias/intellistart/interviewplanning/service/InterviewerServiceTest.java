f48f6410cec54679f029f81ef8770806beddb027
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-11-20 13:06:09 +0200

    Close mock in InterviewerServiceTest

--- src/test/java/com/intellias/intellistart/interviewplanning/service/InterviewerServiceTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/service/InterviewerServiceTest.java
@@ -26,6 +26,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -56,6 +57,12 @@ class InterviewerServiceTest {
         Mockito.CALLS_REAL_METHODS);
 
   }
+
+  @AfterAll
+  static void closeResources() {
+    localDateMock.close();
+  }
+
 
   @Test
   void createSlotWorkingProperly() {
