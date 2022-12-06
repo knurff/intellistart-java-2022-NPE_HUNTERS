d431a0b84358a7a5dc2f509f646f47b1dffcc3be
Famezzs <Andrii.Kozubenko.IT.2021@lpnu.ua>
2022-12-06 23:09:36 +0200

    Try fix dashboard issue

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/dto/DashboardDayDto.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/dto/DashboardDayDto.java
@@ -3,8 +3,12 @@ package com.intellias.intellistart.interviewplanning.controller.dto;
 import com.intellias.intellistart.interviewplanning.model.Booking;
 import com.intellias.intellistart.interviewplanning.model.CandidateSlot;
 import com.intellias.intellistart.interviewplanning.model.InterviewerSlot;
+
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,9 +16,28 @@ import lombok.Getter;
  * A dto representing all bookings, coordinator and candidate slots for a particular day.
  */
 @Getter
-@AllArgsConstructor
 public class DashboardDayDto {
-  private final Map<InterviewerSlot, Set<Long>> bookingIdsByInterviewerSlot;
-  private final Map<CandidateSlot, Set<Long>> bookingIdsByCandidateSlot;
-  private final Map<Long, Booking> bookingsByIds;
+    private final Set<InterviewerSlot> interviewerSlots;
+    private final Map<Long, Set<Long>> bookingIdsByInterviewerSlotId = new HashMap<>();
+    private final Set<CandidateSlot> candidateSlots;
+    private final Map<Long, Set<Long>> bookingIdsByCandidateSlotId = new HashMap<>();
+    private final Map<Long, Booking> bookingsByIds;
+
+    public DashboardDayDto(final Map<InterviewerSlot, Set<Long>> bookingIdsByInterviewerSlot,
+                           final Map<CandidateSlot, Set<Long>> bookingIdsByCandidateSlot,
+                           final Map<Long, Booking> bookingsByIds) {
+
+        this.bookingsByIds = bookingsByIds;
+
+        interviewerSlots = bookingIdsByInterviewerSlot.keySet();
+        candidateSlots = bookingIdsByCandidateSlot.keySet();
+
+        interviewerSlots.stream()
+                .collect(Collectors.toMap(InterviewerSlot::getId,
+                        slot -> bookingIdsByInterviewerSlot.get(slot.getId())));
+
+        candidateSlots.stream()
+                .collect(Collectors.toMap(CandidateSlot::getId,
+                        slot -> bookingIdsByCandidateSlotId.get(slot.getId())));
+    }
 }
