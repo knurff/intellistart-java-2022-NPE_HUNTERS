14b04d633b9f46c7953dcb6aedd0c3072b03692c
Roman Prokopchuk <71777440+ram333n@users.noreply.github.com>
2022-12-07 17:17:14 +0200

    Fix checkstyle issues

--- src/main/java/com/intellias/intellistart/interviewplanning/controller/dto/DashboardDayDto.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controller/dto/DashboardDayDto.java
@@ -3,11 +3,9 @@ package com.intellias.intellistart.interviewplanning.controller.dto;
 import com.intellias.intellistart.interviewplanning.model.Booking;
 import com.intellias.intellistart.interviewplanning.model.CandidateSlot;
 import com.intellias.intellistart.interviewplanning.model.InterviewerSlot;
-
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import lombok.Getter;
 
 /**
@@ -15,27 +13,35 @@ import lombok.Getter;
  */
 @Getter
 public class DashboardDayDto {
-    private final Set<InterviewerSlot> interviewerSlots;
-    private final Map<Long, Set<Long>> bookingIdsByInterviewerSlotId;
-    private final Set<CandidateSlot> candidateSlots;
-    private final Map<Long, Set<Long>> bookingIdsByCandidateSlotId;
-    private final Map<Long, Booking> bookingsByIds;
 
-    public DashboardDayDto(final Map<InterviewerSlot, Set<Long>> bookingIdsByInterviewerSlot,
-                           final Map<CandidateSlot, Set<Long>> bookingIdsByCandidateSlot,
-                           final Map<Long, Booking> bookingsByIds) {
+  private final Set<InterviewerSlot> interviewerSlots;
+  private final Map<Long, Set<Long>> bookingIdsByInterviewerSlotId;
+  private final Set<CandidateSlot> candidateSlots;
+  private final Map<Long, Set<Long>> bookingIdsByCandidateSlotId;
+  private final Map<Long, Booking> bookingsByIds;
 
-        this.bookingsByIds = bookingsByIds;
+  /**
+   * Constructs dashboard by following maps.
+   *
+   * @param bookingIdsByInterviewerSlot map with booking ids of specific interviewer slot
+   * @param bookingIdsByCandidateSlot map with booking ids of specific candidate slot
+   * @param bookingsByIds map with bookings of specific id
+   */
+  public DashboardDayDto(final Map<InterviewerSlot, Set<Long>> bookingIdsByInterviewerSlot,
+      final Map<CandidateSlot, Set<Long>> bookingIdsByCandidateSlot,
+      final Map<Long, Booking> bookingsByIds) {
 
-        interviewerSlots = bookingIdsByInterviewerSlot.keySet();
-        candidateSlots = bookingIdsByCandidateSlot.keySet();
+    this.bookingsByIds = bookingsByIds;
 
-        bookingIdsByInterviewerSlotId = interviewerSlots.stream()
-                .collect(Collectors.toMap(InterviewerSlot::getId,
-                        bookingIdsByInterviewerSlot::get));
+    interviewerSlots = bookingIdsByInterviewerSlot.keySet();
+    candidateSlots = bookingIdsByCandidateSlot.keySet();
 
-        bookingIdsByCandidateSlotId = candidateSlots.stream()
-                .collect(Collectors.toMap(CandidateSlot::getId,
-                        bookingIdsByCandidateSlot::get));
-    }
+    bookingIdsByInterviewerSlotId = interviewerSlots.stream()
+        .collect(Collectors.toMap(InterviewerSlot::getId,
+            bookingIdsByInterviewerSlot::get));
+
+    bookingIdsByCandidateSlotId = candidateSlots.stream()
+        .collect(Collectors.toMap(CandidateSlot::getId,
+            bookingIdsByCandidateSlot::get));
+  }
 }
